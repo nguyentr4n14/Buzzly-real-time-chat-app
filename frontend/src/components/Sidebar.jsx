e4d0beafb6ab67a2047221d8142d273b368968d6
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-01-23 22:55:27 +0200

    Add homepage-chat container

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -32,7 +32,7 @@ const Sidebar = () => {
                 {users.map((user) => (
                     <button
                         key={user._id}
-                        onClick={() => setSelectedUser()}
+                        onClick={() => setSelectedUser(user)}
                         className={`w-full p-3 flex items-center gap-3 hover:bg-base-300 transition-colors ${
                             selectedUser?._id === user._id
                                 ? 'bg-base-300 ring-1 ring-base-300'
